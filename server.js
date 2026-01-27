6cfaca4bad390efb0399b13ed844a74ee6a7bd7f
SurajSingh9696 <surajsingh969670@gmail.com>
2026-01-27 19:35:20 +0530

    Updation

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(cors({
-  origin: ['http://localhost:3000'],
+  origin: 'https://crackit-interview-ai.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
